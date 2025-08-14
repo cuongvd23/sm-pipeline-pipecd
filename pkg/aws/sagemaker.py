9a01efdae8e2c803822214ee3e9b6a24737c5a79
Cuong Vu <johnvu23.vn@gmail.com>
2025-08-14 19:05:20 +0900

    feat: patch sagemaker.local.image._SageMakerContainer

--- pkg/aws/sagemaker.py
+++ pkg/aws/sagemaker.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import boto3
 import sagemaker.local.image
+import yaml
 from sagemaker import Session
 from sagemaker.local.local_session import LocalSession
 
@@ -19,8 +20,40 @@ class ContainerVolume(sagemaker.local.image._Volume):
     ) -> None:
         cwd = os.getenv("HOST_CWD")
         if cwd:
+            logger.warning("Patching container volume")
             host_dir = cwd + host_dir
         super().__init__(host_dir, container_dir, channel)  # type: ignore[no-untyped-call]
+
+
+class SageMakerContainer(sagemaker.local.image._SageMakerContainer):
+    def _generate_compose_file(
+        self,
+        command: str,
+        additional_volumes: list[str] | None = None,
+        additional_env_vars: dict[str, str] | None = None,
+    ) -> dict[str, Any]:
+        content: dict[str, Any] = super()._generate_compose_file(
+            command, additional_volumes, additional_env_vars
+        )  # type: ignore[no-untyped-call]
+
+        if content.get("networks", {}).get("sagemaker-local"):
+            logger.warning("Patching docker-compose.yaml")
+
+            if content.get("version"):
+                del content["version"]
+
+            content["networks"]["sagemaker-local"]["external"] = True
+
+            docker_compose_path = os.path.join(
+                self.container_root or "",
+                sagemaker.local.image.DOCKER_COMPOSE_FILENAME,
+            )
+
+            yaml_content = yaml.dump(content, default_flow_style=False)
+            with open(docker_compose_path, "w") as f:
+                f.write(yaml_content)
+
+        return content
 
 
 def get_session(config: PipelineConfig, sagemaker_config: dict[str, Any]) -> Session:
@@ -32,7 +65,11 @@ def get_session(config: PipelineConfig, sagemaker_config: dict[str, Any]) -> Ses
             default_bucket=config.s3_bucket,
             default_bucket_prefix=config.s3_prefix,
         )
+        sagemaker.local.entities._SageMakerContainer = SageMakerContainer  # type: ignore[misc]
+        sagemaker.local.image._SageMakerContainer = SageMakerContainer  # type: ignore[misc]
+        sagemaker.local.local_session._SageMakerContainer = SageMakerContainer  # type: ignore[misc]
         sagemaker.local.image._Volume = ContainerVolume  # type: ignore[misc]
+        sagemaker.local.image.S3_ENDPOINT_URL_ENV_NAME = "AWS_ENDPOINT_URL_S3"
         session.sagemaker_config = sagemaker_config
         session.config = {"local": {"local_code": True}}
         return session
